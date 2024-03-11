b36ac685a6657093af5ed6957491700b26417e1a
Boris Pavel <boris.pavel@gmx.com>
2024-03-11 14:26:59 +0200

    add authentication

--- app.py
+++ app.py
@@ -1,3 +1,14 @@
+import random
+from flask import Flask, render_template, request, session, redirect
+from flask_session import Session
+from werkzeug.security import check_password_hash, generate_password_hash
+from cs50 import SQL
+from helpers import (
+    eliminate_column_duplicates,
+    expand_crown,
+    calculate_winnings,
+    clear_winnings,
+)
 from symbols import (
     SEVEN,
     CHERRY,
@@ -10,11 +21,8 @@ from symbols import (
     DOLLAR,
     CROWN,
     BELL,
-    Symbol
+    Symbol,
 )
-from flask import Flask, render_template, request
-import random
-from helpers import eliminate_column_duplicates, expand_crown, calculate_winnings, clear_winnings
 
 SYMBOLS = [
     CROWN,
@@ -34,15 +42,29 @@ SYMBOLS = [
 # Configure application
 app = Flask(__name__)
 
-# Set debug mode
-if __name__ == "__main__":
-    app.run(debug=True)
+# Configure session to use filesystem
+app.config["SESSION_PERMANENT"] = False
+app.config["SESSION_TYPE"] = "filesystem"
+Session(app)
+
+# Configure CS50 Library to use SQLite database
+db = SQL("sqlite:///crown.db")
 
 
 @app.route("/", methods=["GET", "POST"])
 def index():
     if request.method == "POST":
         # Generate slot
+        if "user_id" not in session.keys():
+            return render_template(
+                "index_error.html",
+                game=[
+                    [SEVEN, CROWN, SEVEN, PLUM, ORANGE],
+                    [SEVEN, CROWN, SEVEN, SEVEN, STAR],
+                    [SEVEN, CROWN, BELL, ORANGE, CHERRY],
+                ],
+                error="You must log in to play.",
+            )
         game = []
         for i in range(3):
             line = []
@@ -69,18 +91,95 @@ def index():
         expand_crown(game)
         clear_winnings(game)
         win = sum(calculate_winnings(game, 5))
-        return render_template("index.html", game=game, balance=0, win=win)
+        balance = db.execute(
+            "SELECT balance FROM users WHERE id = ?", session["user_id"]
+        )[0]["balance"]
+        return render_template("game.html", game=game, balance=balance, win=win)
     else:
-        return render_template(
-            "index.html",
-            game=[
-                [SEVEN, CROWN, SEVEN, PLUM, ORANGE],
-                [SEVEN, CROWN, SEVEN, SEVEN, STAR],
-                [SEVEN, CROWN, BELL, ORANGE, CHERRY],
-            ],
-            balance=0,
+        if "user_id" in session.keys():
+            balance = db.execute(
+                "SELECT balance FROM users WHERE id = ?", session["user_id"]
+            )[0]["balance"]
+            return render_template(
+                "game.html",
+                game=[
+                    [SEVEN, CROWN, SEVEN, PLUM, ORANGE],
+                    [SEVEN, CROWN, SEVEN, SEVEN, STAR],
+                    [SEVEN, CROWN, BELL, ORANGE, CHERRY],
+                ],
+                balance=balance,
+            )
+        else:
+            return render_template(
+                "index.html",
+                game=[
+                    [SEVEN, CROWN, SEVEN, PLUM, ORANGE],
+                    [SEVEN, CROWN, SEVEN, SEVEN, STAR],
+                    [SEVEN, CROWN, BELL, ORANGE, CHERRY],
+                ]
+            )
+
+
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    session.clear()
+
+    if request.method == "POST":
+
+        # Ensure username was submitted
+        if not request.form.get("username"):
+            return render_template("login_error.html", error="Must provide username.")
+        # Ensure password was submitted
+        elif not request.form.get("password"):
+            return render_template("login_error.html", error="Must provide password.")
+
+        # Query database for username
+        rows = db.execute(
+            "SELECT * FROM users WHERE username = ?", request.form.get("username")
         )
 
+        # Ensure username exists and password is correct
+        if len(rows) != 1 or not check_password_hash(
+            rows[0]["hash"], request.form.get("password")
+        ):
+            return render_template(
+                "login_error.html", error="Invalid username and/or password."
+            )
 
-if __name__ == "__main__":
-    app.run(debug=True)
+        # Remember which user has logged in
+        session["user_id"] = rows[0]["id"]
+
+        # Redirect user to home page
+        return redirect("/")
+
+    return render_template("login.html")
+
+
+@app.route("/register", methods=["GET", "POST"])
+def register():
+    if request.method == "POST":
+        username = request.form.get("username")
+        password = request.form.get("password")
+        confirm = request.form.get("confirm")
+        if password != confirm:
+            return render_template(
+                "register_error.html", error="Password do not match."
+            )
+        if len(db.execute("SELECT * FROM users WHERE username = ?", username)):
+            return render_template("register_error.html", error="Username exists.")
+        else:
+            db.execute(
+                "INSERT INTO users (username, hash) VALUES(?, ?)",
+                username,
+                generate_password_hash(password),
+            )
+            return redirect("/login")
+    else:
+        return render_template("register.html")
+
+
+@app.route("/logout")
+def logout():
+    # Forget any user_id
+    session.clear()
+    return redirect("/")
--- static/script.js
+++ static/script.js
@@ -1,25 +1,41 @@
 const symbols = document.querySelectorAll(".symbol");
 
-setInterval(() => {
-    for (let s of symbols) {
-        let style = s.style;
-        style.animation = "none";
-        setTimeout(() => {
-            style.animation = "bounce 1s";
-        }, 10);
-    }
-}, 10000);
+if (symbols) {
+    setInterval(() => {
+        for (let s of symbols) {
+            let style = s.style;
+            style.animation = "none";
+            setTimeout(() => {
+                style.animation = "bounce 1s";
+            }, 10);
+        }
+    }, 10000);
+}
+
 
 const form = document.querySelector("#spin");
-form.addEventListener('submit', (e) => {
-    e.preventDefault();
+if (form) {
+    form.addEventListener('submit', (e) => {
+        e.preventDefault();
 
-    for (let s of symbols) {
-        s.style.animation = "bounceOutDown 1s"
-    }
+        for (let s of symbols) {
+            s.style.animation = "bounceOutDown 1s"
+        }
 
-    // Delay form submission
-    setTimeout(() => {
-        e.target.submit();
-    }, 900);
-});
+        // Delay form submission
+        setTimeout(() => {
+            e.target.submit();
+        }, 900);
+    });
+
+}
+
+
+const toastLiveExample = document.getElementById('liveToast')
+if (toastLiveExample)
+{
+    document.addEventListener('DOMContentLoaded', () => {
+        const toastBootstrap = bootstrap.Toast.getOrCreateInstance(toastLiveExample)
+        toastBootstrap.show()
+    });
+}
